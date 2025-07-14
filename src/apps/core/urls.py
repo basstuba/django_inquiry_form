fa0360fb1a9e349710cc92a26f58b879cc902b75
nutka <tuba4080@outlook.com>
2025-07-14 22:33:49 +0200

    ログインページ・新規登録ページ完成

--- src/apps/core/urls.py
+++ src/apps/core/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.top_view, name='top'),
     path('confirm/', views.confirm_view, name='confirm'),
     path('thanks/', views.thanks_view, name='thanks'),
+    path('edit/', views.edit_view, name='edit'),
 ]
--- src/apps/core/views.py
+++ src/apps/core/views.py
@@ -3,13 +3,24 @@ from django.views.decorators.http import require_http_methods
 from .forms import CreateInquiryForm
 from .models import Contact
 
+GENDER_DISPLAY = {
+    "1": "男性",
+    "2": "女性",
+    "3": "未回答"
+}
+
 # css記述が終了したらコメントアウト解除
 # @require_http_methods(["GET", "POST"])
 def top_view(request):
     if request.method == "POST":
         form = CreateInquiryForm(request.POST)
         if form.is_valid(): # 保存せずにそのまま確認画面へ
-            return render(request, 'core/confirm.html', {'form': form})
+            gender_label = GENDER_DISPLAY.get(form.cleaned_data['gender'])
+            context = {
+                'form': form,
+                'gender_label': gender_label,
+            }
+            return render(request, 'core/confirm.html', context)
     else:
         form = CreateInquiryForm()
 
@@ -35,3 +46,9 @@ def confirm_view(request):
 # @require_http_methods(["GET"])
 def thanks_view(request):
     return render(request, 'core/thanks.html')
+
+
+# @require_http_methods(["POST"])
+def edit_view(request):
+    form = CreateInquiryForm(request.POST)
+    return render(request, 'core/top.html', {'form': form})
