4319cf7d4c6da7ccb2ad2e6114c553d0651cb38b
nutka <tuba4080@outlook.com>
2025-07-17 13:25:47 +0200

    入力確認ページ完成

--- src/apps/core/views.py
+++ src/apps/core/views.py
@@ -4,21 +4,21 @@ from .forms import CreateInquiryForm
 from .models import Contact
 
 GENDER_DISPLAY = {
-    "1": "男性",
-    "2": "女性",
-    "3": "未回答"
+    1: "男性",
+    2: "女性",
+    3: "未回答"
 }
 
 CATEGORY_DISPLAY = {
-    "1": "商品のお届けについて",
-    "2": "商品の交換について",
-    "3": "商品トラブル",
-    "4": "ショップへのお問い合わせ",
-    "5": "その他"
+    1: "商品のお届けについて",
+    2: "商品の交換について",
+    3: "商品トラブル",
+    4: "ショップへのお問い合わせ",
+    5: "その他"
 }
 
-# css記述が終了したらコメントアウト解除
-# @require_http_methods(["GET", "POST"])
+
+@require_http_methods(["GET", "POST"])
 def top_view(request):
     if request.method == "POST":
         form = CreateInquiryForm(request.POST)
@@ -42,7 +42,7 @@ def top_view(request):
     return render(request, 'core/top.html', context)
 
 
-# @require_http_methods(["POST"])
+@require_http_methods(["POST"])
 def confirm_view(request):
     form = CreateInquiryForm(request.POST)
     if form.is_valid(): # ここで保存
@@ -53,12 +53,17 @@ def confirm_view(request):
     return redirect('core:top')
 
 
-# @require_http_methods(["GET"])
+@require_http_methods(["GET"])
 def thanks_view(request):
     return render(request, 'core/thanks.html')
 
 
-# @require_http_methods(["POST"])
+@require_http_methods(["POST"])
 def edit_view(request):
     form = CreateInquiryForm(request.POST)
-    return render(request, 'core/top.html', {'form': form})
+    context = {
+        'form': form,
+        'gender_choices': Contact.GENDER_CHOICES,
+        'category_choices': Contact.CATEGORY_CHOICES,
+    }
+    return render(request, 'core/top.html', context)
